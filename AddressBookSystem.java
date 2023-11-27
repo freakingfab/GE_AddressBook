89da1c24e43bf9db6c6a82861bba355bdfcc9c6a
Daksh <dakshnahar2001@gmail.com>
2023-11-27 20:06:21 +0530

    Added: UC6 multiple address books crated

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -86,9 +86,11 @@ class Contact{
 //@desc: list of contacts is stored
 class AddressBook{
     private List<Contact> contacts;
+    private String bookName;
 
 //    @desc: constructor for class
-    public AddressBook(){
+    public AddressBook(String bookName){
+        this.bookName=bookName;
         contacts = new ArrayList<>();
     }
 
@@ -141,97 +143,99 @@ public class AddressBookSystem {
     private static final int editContact = 3;
     private static final int deleteContact=4;
     private static final int exit =5;
-
-    public static void main(String[] args){
+    private static List<AddressBook> addressBooks = new ArrayList<>(); ;
+    public static void main(String[] args) {
         System.out.println("Welcome to Address Book System");
         Scanner input = new Scanner(System.in);
-        int choice=-1;
-        AddressBook contactBook = new AddressBook();
-        while(true){
-            System.out.println("======================");
-            System.out.println("Add Contact -> 1");
-            System.out.println("Display Contacts -> 2");
-            System.out.println("Edit Contact ->3");
-            System.out.println("Delete Contact -> 4");
-            System.out.println("Exit-> 5");
-            System.out.print("Please Enter the choice: ");
-            choice = input.nextInt();
-            if(choice==exit){
-                break;
-            }
-            switch (choice){
-                case addContact:
-                    System.out.print("Enter First Name: ");
-                    String firstName = input.next();
-                    System.out.print("Enter Last Name: ");
-                    String lastName = input.next();
-                    System.out.print("Enter Address: ");
-                    String address = input.next();
-                    System.out.print("Enter State: ");
-                    String state = input.next();
-                    System.out.print("Enter City: ");
-                    String city = input.next();
-                    System.out.print("Enter email: ");
-                    String email = input.next();
-                    System.out.print("Enter ZipCode: ");
-                    int zipCode = input.nextInt();
-                    System.out.print("Enter Phone Number: ");
-                    long phoneNo = input.nextLong();
-                    Contact C = new Contact(firstName,lastName,address,city,state,email,zipCode,phoneNo);
-                    contactBook.addContact(C);
+        AddressBook contactBook1 = new AddressBook("Book1");
+        addressBooks.add(contactBook1);
+        AddressBook contactBook2 = new AddressBook("Book2");
+        addressBooks.add(contactBook2);
+        for (AddressBook contactBook: addressBooks) {
+            int choice = -1;
+            while (true) {
+                System.out.println("======================");
+                System.out.println("Add Contact -> 1");
+                System.out.println("Display Contacts -> 2");
+                System.out.println("Edit Contact ->3");
+                System.out.println("Delete Contact -> 4");
+                System.out.println("Exit-> 5");
+                System.out.print("Please Enter the choice: ");
+                choice = input.nextInt();
+                if (choice == exit) {
                     break;
+                }
+                switch (choice) {
+                    case addContact:
+                        System.out.print("Enter First Name: ");
+                        String firstName = input.next();
+                        System.out.print("Enter Last Name: ");
+                        String lastName = input.next();
+                        System.out.print("Enter Address: ");
+                        String address = input.next();
+                        System.out.print("Enter State: ");
+                        String state = input.next();
+                        System.out.print("Enter City: ");
+                        String city = input.next();
+                        System.out.print("Enter email: ");
+                        String email = input.next();
+                        System.out.print("Enter ZipCode: ");
+                        int zipCode = input.nextInt();
+                        System.out.print("Enter Phone Number: ");
+                        long phoneNo = input.nextLong();
+                        Contact C = new Contact(firstName, lastName, address, city, state, email, zipCode, phoneNo);
+                        contactBook.addContact(C);
+                        break;
 
-                case displayContact:
-                    displayBook(contactBook.getContacts());
-                    break;
-                case editContact:
-                    System.out.print("Enter First Name: ");
-                    firstName = input.next();
-                    System.out.print("Enter Last Name: ");
-                    lastName = input.next();
-                    int cuC=getContact(contactBook.getContacts(), firstName, lastName);
-                    if(cuC!=-1){
+                    case displayContact:
+                        displayBook(contactBook.getContacts());
+                        break;
+                    case editContact:
                         System.out.print("Enter First Name: ");
                         firstName = input.next();
                         System.out.print("Enter Last Name: ");
                         lastName = input.next();
-                        System.out.print("Enter Address: ");
-                        address = input.next();
-                        System.out.print("Enter State: ");
-                        state = input.next();
-                        System.out.print("Enter City: ");
-                        city = input.next();
-                        System.out.print("Enter email: ");
-                        email = input.next();
-                        System.out.print("Enter ZipCode: ");
-                        zipCode = input.nextInt();
-                        System.out.print("Enter Phone Number: ");
-                        phoneNo = input.nextLong();
-                        contactBook.editContact(cuC, firstName,lastName,address,city,state,email,zipCode,phoneNo);
-                    }
-                    else{
-                        System.out.print("Contact Not Found");
-                    }
-                    break;
-                case deleteContact:
-                    System.out.print("Enter First Name: ");
-                    firstName = input.next();
-                    System.out.print("Enter Last Name: ");
-                    lastName = input.next();
-                    int index=getContact(contactBook.getContacts(), firstName, lastName);
-                    if(index!=-1){
-                        contactBook.deleteContact(index);
-                    }
-                    else{
-                        System.out.println("Contact Not Found");
-                    }
-                    break;
-                default:
-                    System.out.println("Invalid input");
+                        int cuC = getContact(contactBook.getContacts(), firstName, lastName);
+                        if (cuC != -1) {
+                            System.out.print("Enter First Name: ");
+                            firstName = input.next();
+                            System.out.print("Enter Last Name: ");
+                            lastName = input.next();
+                            System.out.print("Enter Address: ");
+                            address = input.next();
+                            System.out.print("Enter State: ");
+                            state = input.next();
+                            System.out.print("Enter City: ");
+                            city = input.next();
+                            System.out.print("Enter email: ");
+                            email = input.next();
+                            System.out.print("Enter ZipCode: ");
+                            zipCode = input.nextInt();
+                            System.out.print("Enter Phone Number: ");
+                            phoneNo = input.nextLong();
+                            contactBook.editContact(cuC, firstName, lastName, address, city, state, email, zipCode, phoneNo);
+                        } else {
+                            System.out.print("Contact Not Found");
+                        }
+                        break;
+                    case deleteContact:
+                        System.out.print("Enter First Name: ");
+                        firstName = input.next();
+                        System.out.print("Enter Last Name: ");
+                        lastName = input.next();
+                        int index = getContact(contactBook.getContacts(), firstName, lastName);
+                        if (index != -1) {
+                            contactBook.deleteContact(index);
+                        } else {
+                            System.out.println("Contact Not Found");
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid input");
+                }
             }
         }
     }
-
 
 //    @desc: function for displaying contacts
 //    @params: List of Contact object
